874fe21d37b9e1420fb30d241c30910136da3ce4
bachhoalieuloan <120135256+bachhoalieuloan@users.noreply.github.com>
2024-11-04 21:32:24 +0700

    Update config.js

--- 69shu/src/config.js
+++ 69shu/src/config.js
@@ -1,5 +1,5 @@
-//    var host = 'https://www.69shuba.com';
-let BASE_URL = 'https://69shuba.cx';
+//    var host = 'https://www.69shuba.cx';
+let BASE_URL = 'https://69shu.biz';
 try {
     if (CONFIG_URL) {
         BASE_URL = CONFIG_URL;
